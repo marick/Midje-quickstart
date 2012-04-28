df18e134304daa5b9e28ad5216019944a2a1beff
Brian Marick <marick@exampler.com>
2012-04-28 18:07:55 -0500

    Do we actually need source directories?

--- checkers/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject checkers "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.4.0-beta1"]])
